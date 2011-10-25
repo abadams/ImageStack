da394bb4e7570fc2daa3676d48ad5901b8a8e444
cyberfluorescent@gmail.com <cyberfluorescent@gmail.com@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2011-10-25 19:48:07 +0000

    Implemented Cho and Lee, SIGGRAPH Asia 2009 blind deconvolution

---
+++ src/KernelEstimation.h
@@ -0,0 +1,23 @@
+#ifndef KERNELESTIMATION_H
+#define KERNELESTIMATION_H
+#include "header.h"
+
+class KernelEstimation : public Operation {
+ public:
+  void help();
+  void parse(vector<string> args);
+  static Image apply(Window im, int kernel_size = 9);
+
+ private:
+  static void ShockFilterIteration(Window im, float dt = 1.f);
+  static void BilateralFilterIteration(Window im, float sigma_r);
+  static float DotProduct(Window im1, Window im2,
+			  int channel = 0, int frame = 0);
+  static void NormalizeSum(Window im);
+  static Image EnlargeKernel(Window im, int w, int h);
+  static Image ContractKernel(Window im, int size);
+  static Image BilinearResample(Image im, int w, int h);
+};
+
+#include "footer.h"
+#endif
