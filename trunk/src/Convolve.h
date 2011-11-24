1ec39d53a067299f94e686971781dbc5f08ee79b
andrew.b.adams <andrew.b.adams@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2011-11-24 04:15:41 +0000

    Cleaned up and merged some of the deconvolution operators.

--- trunk/src/Convolve.h
+++ trunk/src/Convolve.h
@@ -17,20 +17,5 @@ public:
 
 };
 
-
-class Deconvolve : public Operation {
-public:
-    void help();
-    void parse(vector<string> args);
-    static Image apply(Window im, Window filter, float maxTime = -1, int maxIterations = -1);
-    static Image apply(Window im, Window filter, Window initialGuess, float maxTime = -1, int maxIterations = -1);
-private:
-    static double dot(Image &a, Image &b);
-    static double norm(Image &a);
-
-    // out = alpha * a + b
-    static void scaleAdd(Image out, float alpha, Image &a, Image &b);
-};
-
 #include "footer.h"
 #endif
--- trunk/src/KernelEstimation.h
+++ trunk/src/KernelEstimation.h
@@ -1,25 +1,27 @@
+#ifndef NO_FFTW
 #ifndef KERNELESTIMATION_H
 #define KERNELESTIMATION_H
 #include "header.h"
 
 class KernelEstimation : public Operation {
- public:
-  void help();
-  void parse(vector<string> args);
-  static Image apply(Window im, int kernel_size = 9);
+  public:
+    void help();
+    void parse(vector<string> args);
+    static Image apply(Window im, int kernel_size = 9);
 
-  // Helpers
-  static void NormalizeSum(Window im);
-  static Image EnlargeKernel(Window im, int w, int h);
-  static Image ContractKernel(Window im, int size);
-  static Image BilinearResample(Image im, int w, int h);
+    // Helpers
+    static void NormalizeSum(Window im);
+    static Image EnlargeKernel(Window im, int w, int h);
+    static Image ContractKernel(Window im, int size);
+    static Image BilinearResample(Image im, int w, int h);
 
- private:
-  static void ShockFilterIteration(Window im, float dt = 1.f);
-  static void BilateralFilterIteration(Window im, float sigma_r);
-  static float DotProduct(Window im1, Window im2,
-			  int channel = 0, int frame = 0);
+  private:
+    static void ShockFilterIteration(Window im, float dt = 1.f);
+    static void BilateralFilterIteration(Window im, float sigma_r);
+    static float DotProduct(Window im1, Window im2,
+                            int channel = 0, int frame = 0);
 };
 
 #include "footer.h"
+#endif
 #endif
