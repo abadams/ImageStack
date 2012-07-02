5f2dfbebd39cfed127071d86bbdaca7a6358a55a
andrew.b.adams <andrew.b.adams@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2012-07-02 18:07:36 +0000

    Updated example plugin

--- examples/plugin.cpp
+++ examples/plugin.cpp
@@ -5,10 +5,10 @@
 // ld -dylib plugin.o -o plugin.so -undefined dynamic_lookup
 //
 // On linux you compile it like this:
-// g++ -shared plugin.cpp -o plugin.so -I ../src -fPIC
+// g++ -std=gnu++0x -shared plugin.cpp -o plugin.so -I ../src -fPIC
 //
 // And use it like this:
-// ImageStack -load ../pics/dog1.jpg -plugin ./plugin.so -help foo -foo -display
+// ImageStack -load ../pics/dog1.jpg -plugin ./plugin.so -help foo -test foo -foo -display
 
 #include <ImageStack.h>
 
@@ -24,31 +24,51 @@ public:
         push(im);
     }
 
-    // Foo doubles an image and then squares it
-    static Image apply(Window im) {
+    // Foo doubles an image then squares it, then flips it
+    static Image apply(Image im) {
         Image out(im);
 
-        // Call an existing imagestack operator
-        Add::apply(out, out);
+        // Use an image expression
+        out += out;
 
         // Do some custom stuff
         for (int t = 0; t < out.frames; t++) {
             for (int y = 0; y < out.height; y++) {
                 for (int x = 0; x < out.width; x++) {
                     for (int c = 0; c < out.channels; c++) {
-                        out(x, y, t)[c] *= out(x, y, t)[c];
+                        out(x, y, t, c) *= out(x, y, t, c);
                     }
                 }
             }
         }
 
+        // Use an imagestack operator
+        Flip::apply(out, 'x');
+
         return out;
     }
 
     void help() {
-        pprintf("Foo help\n");
+        pprintf("-foo doubles an image, squares it, and flips it.\n");
+    }
+
+    bool test() {
+        // Apply foo to a linear ramp
+        Image ramp(128, 128, 1, 1);
+        Lazy::X x; Lazy::Y y;
+        ramp.set(x + 3*y);
+        Image out = Foo::apply(ramp);
+
+        // For a ramp the output should be...
+        Image correct(128, 128, 1, 1);
+        correct.set((ramp.width-1-x) + 3*y);
+        correct *= 2;
+        correct *= correct;
+        
+        return nearlyEqual(out, correct);
     }
 };
+
 
 extern "C" void init_imagestack_plugin(map<string, Operation *> &operationMap) {
     printf("Initializing ImageStack plugin\n");
