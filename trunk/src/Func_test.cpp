a16b85fa04c562ae30a6efa29e8101e852c75ac6
andrew.b.adams <andrew.b.adams@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2012-07-08 23:44:31 +0000

    Several bugfixes. Performance now as expected. inline is best for low-compute. chunk is best for high-compute.

--- trunk/src/Func_test.cpp
+++ trunk/src/Func_test.cpp
@@ -9,14 +9,14 @@ int main(int argc, char **argv) {
 
     try {
         Image in = Load::apply("in.tmp");       
-        Image up(in.width*2, in.height, in.frames, in.channels);
+        Image up(in.width, in.height, in.frames, in.channels);
 
         double t1 = currentTime();
         printf("Inline...\n");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             // blur the square root
             // inline
-            auto tmp = zeroBoundary(sqrt(in));
+            auto tmp = zeroBoundary((in));
             auto bx = shiftX(tmp, -1) + 2*tmp + shiftX(tmp, 1);
             auto by = shiftY(bx, -1) + 2*bx + shiftY(bx, 1);
             up.set(by);
@@ -24,9 +24,9 @@ int main(int argc, char **argv) {
         double t2 = currentTime();
 
         printf("Root...\n");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             // root
-            Image tmp = sqrt(in);
+            Image tmp = (in);
             Image bx = shiftX(zeroBoundary(tmp), -1) + 2*tmp + shiftX(zeroBoundary(tmp), 1);
             Image by = shiftY(zeroBoundary(bx), -1) + 2*bx + shiftY(zeroBoundary(bx), 1);
             up = by;
@@ -34,14 +34,13 @@ int main(int argc, char **argv) {
         double t3 = currentTime();
 
         printf("Chunk...\n");        
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             // chunk
-            Func tmp = zeroBoundary(sqrt(in));
+            Func tmp = zeroBoundary((in));
             Func bx = shiftX(tmp, -1) + 2*tmp + shiftX(tmp, 1);
             Func by = shiftY(bx, -1) + 2*bx + shiftY(bx, 1);
             up.set(by);
         }
-    
         double t4 = currentTime();
 
         printf("%f %f %f\n", t2-t1, t3-t2, t4-t3);
