d740931d43d92321ae03e58e00da6072e3a434ca
andrew.b.adams <andrew.b.adams@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2012-07-08 21:13:21 +0000

    Added a func test to compare inline root and chunk. Does not work

--- trunk/src/Func_test.cpp
+++ trunk/src/Func_test.cpp
@@ -12,28 +12,34 @@ int main(int argc, char **argv) {
         Image up(in.width*2, in.height, in.frames, in.channels);
 
         double t1 = currentTime();
+        printf("Inline...\n");
         for (int i = 0; i < 100; i++) {
-            up.set(interleaveX(in, in)*0.75f + 
-                   interleaveX(shiftX(zeroBoundary(in), 1), 
-                               shiftX(zeroBoundary(in), -1))*0.25f);
+            // blur the square root
+            // inline
+            auto tmp = zeroBoundary(sqrt(in));
+            auto bx = shiftX(tmp, -1) + 2*tmp + shiftX(tmp, 1);
+            auto by = shiftY(bx, -1) + 2*bx + shiftY(bx, 1);
+            up.set(by);
         }
         double t2 = currentTime();
 
-        //Image up(in.width, in.height, in.frames, in.channels);
+        printf("Root...\n");
         for (int i = 0; i < 100; i++) {
-            
-            Image inM = in.selectColumns(1, in.width-2);
-            Image inL = in.selectColumns(0, in.width-2);
-            Image inR = in.selectColumns(2, in.width-2);
-            up.selectColumns(2, 2*in.width-4).set(
-                interleaveX(inM, inM)*0.75f + interleaveX(inL, inR)*0.25f
-                );
+            // root
+            Image tmp = sqrt(in);
+            Image bx = shiftX(zeroBoundary(tmp), -1) + 2*tmp + shiftX(zeroBoundary(tmp), 1);
+            Image by = shiftY(zeroBoundary(bx), -1) + 2*bx + shiftY(zeroBoundary(bx), 1);
+            up = by;
         }
         double t3 = currentTime();
-        
+
+        printf("Chunk...\n");        
         for (int i = 0; i < 100; i++) {
-            Func zb = zeroBoundary(interleaveX(in, in));
-            up.set((shiftX(zb, -1) + 2*zb + shiftX(zb, 1))/4);
+            // chunk
+            Func tmp = zeroBoundary(sqrt(in));
+            Func bx = shiftX(tmp, -1) + 2*tmp + shiftX(tmp, 1);
+            Func by = shiftY(bx, -1) + 2*bx + shiftY(bx, 1);
+            up.set(by);
         }
     
         double t4 = currentTime();
